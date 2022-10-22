3df92417242d5bbd4c01015b611993076a486075
zachrihall <hallzachri@gmail.com>
2022-10-22 03:56:47 -0400

    imported plutolib and removed strictmode

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,6 +1,10 @@
 import "./DashboardPage.scss";
+import * as pluto from 'plutolib'  ;
 
 function DashboardPage() {
+  pluto.container('test','horizontal', 'center', 'blue');
+  pluto.h1('test h1', 'test', 'testh1', 'black');
+
   return (
     <div className="dashboard">
       <h1>This is Dashboard Page</h1>
@@ -45,7 +49,10 @@ function DashboardPage() {
         </div>
         <div className='input__crud'>
           <h3 className='input__crud-title'>Edit</h3>
-          <div className='input__crud-buttons-list'></div>
+          <div className='input__crud-buttons-list'>
+            <button className='input__crud-buttons-list-button'>Create</button>
+            <button className='input__crud-buttons-list-button'>Delete</button>
+          </div>
         </div>
       </section>
     </div>
