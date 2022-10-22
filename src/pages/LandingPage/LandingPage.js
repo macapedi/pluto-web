d26948147245802b3e58c830adeed592a0d8defe
macapedi <macapedi90@gmail.com>
2022-10-22 09:56:54 -0400

    footer created, landing page enlarged

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import "./LandingPage.scss";
 import { Link } from "react-router-dom";
 import matrix from "../../assets/matrix.jpg";
+import Footer from "../../components/Header/Footer/Footer.js";
+
 
 function LandingPage() {
   return (
@@ -12,7 +14,9 @@ function LandingPage() {
             Start coding right now in HTML and CSS with our beginner friendly
             tools.
           </p>
-          <Link className="landing-page__button" to="/dashboard">Get Started</Link>
+          <Link className="landing-page__button" to="/dashboard">
+            Get Started
+          </Link>
         </div>
       </div>
       <div className="landing-page__info">
@@ -36,12 +40,29 @@ function LandingPage() {
         </div>
       </div>
       <div>
-      <img
-        src={matrix}
-        alt="Photo of zeros and ones"
-        className="landing-page__info-img"
-      />
+        <img
+          src={matrix}
+          alt="Photo of zeros and ones"
+          className="landing-page__info-img"
+        />
       </div>
+      <div className="landing-page__title-work">
+        <h1 className="landing-page__work-title">How does this work?</h1>
+      </div>
+      <div className="landing-page__paragraph">
+        <p>
+          Pluto Web is a web-based application that simplifies code-writing by
+          generating a code from a simple process. The process involves guiding
+          the user through: (1) inputting a value, (2) selecting a component,
+          and (3) creating the component. First, the user will input possible
+          values they want in the input field; this may be a text, a link, or an
+          image. Second, the user will choose from the selection of components
+          we included in the application. Finally, when the user press the
+          create button, the application will generate the preview of the code
+          and the actual code that the user can copy and use for their needs.
+        </p>
+      </div>
+      <Footer/>
     </>
   );
 }
